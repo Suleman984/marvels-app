6367cf917e51133b0b13a03342a509ed230aa184
Suleman984 <sulemanefc@gmail.com>
2023-09-12 04:27:37 -0700

    feat:Added ComicSection with  comic books

--- src/Components/atoms/ComicBook.tsx
+++ src/Components/atoms/ComicBook.tsx
@@ -1,15 +1,29 @@
 import { Box, Typography } from "@mui/material";
 import React from "react";
-
- const ComicBook = () => {
+const getRandomTime = () => {
+    // Generate a random number between 1 and 7 for days
+    const randomDays = Math.floor(Math.random() * 7) + 1;
+  
+    // Generate a random number between 1 and 23 for hours
+    const randomHours = Math.floor(Math.random() * 23) + 1;
+  
+    return `${randomDays} days, ${randomHours} hours ago`;
+  };
+const ComicBook = () => {
+    const randomTime = getRandomTime();
   return (
-    <Box style={{ width: "300px", height: "300px"}}>
-      <img src="./Assets/marvel2.jpg" alt="Comics" width="100%" height="100%" />
-      <Box>
-        <Typography >Comics</Typography>
-        <Typography variant="h5">Heelo Comic World</Typography>
+  <>
+    <Box style={{ display: "flex",  width: "100%", height: "200px" }}>
+      <img src="./Assets/marvels2.jpg" alt="Comics" width="300px" height="100%" />
+      <Box marginLeft="16px">
+        <Typography>Comics</Typography>
+        <Typography variant="h5"><a href="#">Hello Comic World</a></Typography>
+        <Typography variant="subtitle1">{randomTime}</Typography>
       </Box>
+      
     </Box>
+    <hr /></>
   );
 };
+
 export default ComicBook;
